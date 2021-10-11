adf8f4ffeadef5e7963fa16c3075e602a522c375
mateussatoh <mateussatoh@gmail.com>
2021-10-10 23:21:01 -0300

    add: eslint & editorconfig

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World') )
+app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(3000, () =>
-   console.log("🔥 Server started at http://localhost:3000")
+    console.log('🔥 Server started at http://localhost:3000')
 );
