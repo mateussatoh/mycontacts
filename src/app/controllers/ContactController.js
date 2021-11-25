aee64a9116eb0664401051088cb3799c7b93e51f
mateussatoh <mateussatoh@gmail.com>
2021-11-25 14:50:42 -0300

    add: order by query param

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -2,12 +2,14 @@ const ContactRepository = require('../repositories/ContactRepository');
 
 class ContactController {
   async index(request, response) {
-    const contacts = await ContactRepository.findAll();
+    const { orderBy } = request.query;
+    const contacts = await ContactRepository.findAll(orderBy);
     response.json(contacts);
   }
 
   async show(request, response) {
     const { id } = request.params;
+
     const contact = await ContactRepository.findById(id);
     if (!contact) {
       return response.status(404).json({ error: 'User not found' });
